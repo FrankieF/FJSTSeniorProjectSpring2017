473cdc0096e7f029c323ac53fde56f2dba0e977f
James A. Donnell Jr <bubbajim715@verizon.net>
2017-03-04 10:54:00 -0500

    Maven dependency and project folder structure for MVC

---
+++ src/groupSPV/controller/package-info.java
@@ -0,0 +1,7 @@
+/** Controller package.
+*
+* @author Frank Fasola
+* @author James Donnell
+* @author Spencer Escalante
+* @author Trevor Silva */
+package groupSPV.controller;
---
+++ src/groupSPV/model/package-info.java
@@ -0,0 +1,7 @@
+/** Model package.
+*
+* @author Frank Fasola
+* @author James Donnell
+* @author Spencer Escalante
+* @author Trevor Silva */
+package groupSPV.model;
