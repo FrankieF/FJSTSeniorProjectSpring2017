63d7299491ccf66c4e675f2765bcd4500563d2e0
silvat1 <silvat1@students.rowan.edu>
2017-03-22 20:23:49 -0400

    Weeks 1+2 Frontend Development for BlockchainGui

---
+++ groupSPV/controller/BlockchainDriver.java
@@ -0,0 +1,43 @@
+package groupSPV.controller;
+
+import java.io.File;
+
+import org.bitcoinj.utils.BriefLogFormatter;
+
+import groupSPV.model.CustomKit;
+
+/** BlockchainDriver is a driver class to initiate the downloading the Blockchain and display it.
+*
+* @author Frank Fasola
+* @author James Donnell
+* @author Spencer Escalante
+* @author Trevor Silva */
+public class BlockchainDriver {
+
+	/** Default save location for any OS. */
+	private static final String saveLocation = getSystemPath() + "SeniorProject_Bitcoin_Client\\";
+	private CustomKit kit;
+	
+	/** Main method.
+	 * @param args Arguments not utilized. */
+	public BlockchainDriver() {
+		BriefLogFormatter.init(); // Sets logging format, default for now
+		kit = new CustomKit(new File(saveLocation));
+
+	}
+	
+	public CustomKit getKit(){
+		return kit;
+	}
+
+	
+	/** Returns User's full 'AppData' path if Windows, blank string if not.
+	 * @return 'AppData' full path. */
+	private static String getSystemPath() {
+		if (System.getenv("appdata") == null) {
+			return "";
+		} else {
+			return System.getenv("appdata") + "\\";
+		}
+	}
+}
